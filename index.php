753166de1f06ec113f3dec55f90f77cff734f9c7
MajorWolf <72295325+Major-Wolf@users.noreply.github.com>
2022-12-07 05:21:46 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<html>
-  123
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DeviceHoster</title>
+</head>
+<body>
+    
+</body>
 </html>
